1ec61816e8877d4a4ed07cffa1446e333de043d1
Jesús Borrero <jesufer2018@hotmail.com>
2023-01-29 01:37:39 -0500

    Block component (with card-like styles)

---
+++ src/components/Block.svelte
@@ -0,0 +1,13 @@
+<section>
+	<slot />
+</section>
+
+<style>
+	section {
+    padding: 1rem;
+    background-color: white;
+    border: 1px solid rgb(var(--accent) / 0.25);
+    border-radius: 0.4rem;
+    line-height: 1.6;
+  }
+</style>
