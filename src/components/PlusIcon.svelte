ae808333aa47308c7cc8bf88f6de1cbedb0438b9
Jesús Borrero <jesufer2018@hotmail.com>
2023-01-29 01:42:01 -0500

    Icons

---
+++ src/components/PlusIcon.svelte
@@ -0,0 +1,14 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+  class="w-8 h-8"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M12 4.5v15m7.5-7.5h-15"
+  />
+</svg>
