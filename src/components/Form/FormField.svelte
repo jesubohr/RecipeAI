f3ed48665badf0ebb253af82d9a9b4aa64b76f08
Jesús Borrero <jesufer2018@hotmail.com>
2023-02-07 03:26:43 -0500

    Add composable label component

---
+++ src/components/Form/FormField.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { useId } from "../../hooks"
+  export let label = ""
+  export let subLabel = ""
+  const id = useId()
+</script>
+
+<div class="flex flex-col gap-2 w-full">
+  <label for={id} class="text-2xl text-secondary font-semibold capitalize"
+  >
+    {label}
+    <span class="text-sm">{subLabel}</span>
+  </label>
+  <slot id={id}></slot>
+</div>
