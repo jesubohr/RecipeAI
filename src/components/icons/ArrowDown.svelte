efa6fd221b51119d858b28a2c6dfc2f2b06de8eb
Jesús Borrero <jesufer2018@hotmail.com>
2023-02-07 03:01:04 -0500

    Compose Icons

---
+++ src/components/icons/ArrowDown.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let active = false
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  class="w-6 h-6 pointer-events-none {active
+    ? 'transform rotate-180'
+    : ''} transition-transform"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M19 9l-7 7-7-7"
+  />
+</svg>
---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as XIcon } from './XIcon.svelte'
+export { default as LoadIcon } from './LoadIcon.svelte'
+export { default as PlusIcon } from './PlusIcon.svelte'
+export { default as ArrowDown } from './ArrowDown.svelte'
