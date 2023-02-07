539933e38e464ebc1e4c3b60f8352c9ecb2e35c6
Jesús Borrero <jesufer2018@hotmail.com>
2023-02-07 03:44:46 -0500

    Change crypto module for custom method

--- src/hooks/useId.ts
+++ src/hooks/useId.ts
@@ -1,3 +1,13 @@
+function generateUUID() {
+  var d = new Date().getTime()
+  var uuid = "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0
+    d = Math.floor(d / 16)
+    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16)
+  })
+  return uuid
+}
+
 export function useId() {
-  return crypto.randomUUID().slice(0, 8)
+  return generateUUID().slice(0, 10)
 }
